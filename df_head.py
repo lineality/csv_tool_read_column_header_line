05e8868543b1383f8e7cff899c538b74f5f43bb9
Geoffrey Gordon Ashbrook <lineality@gmail.com>
2022-08-04 12:38:32 -0400

    Update df_head.py

--- df_head.py
+++ df_head.py
@@ -1,17 +1,41 @@
 """
-Script to print first lines of files in same directory
+### csv_tool_read_column_header_line
+["Save link as" to download] https://raw.githubusercontent.com/lineality/csv_tool_read_column_header_line/main/df_head.py 
 
-e.g.
-Step 1. Put df_head.py file into a folder with you .csv file(s)
-Step 2. open a terminal and type(and hit return): python3 df_head.py
+# df_head.py
 
-vanilla python only, no installs needed:
-This should work for even big files on any computer
-where you can run a python script. no env or package
-installs should be needed
+This is a python script that reads 
+the first line of .csv files.
+e.g. 
+df_head.py mimics the pandas operation: df.head()
 
+## Instruction Steps:
+
+#### Step 1. 
+- Put df_head.py file into a folder 
+with your .csv file(s)
+
+#### Step 2. 
+- open a terminal and type (and hit return): 
+```
+python3 df_head.py
+```
+
+## No Python Environment or Installs Needed
+This script uses only vanilla python and so
+should not require any additional environments,
+libraries, packages or set up. Just run in a folder 
+along with the .csv files.
+
+This method should work for larger files 
+(hundreds of megs or a few gigs in size)  
+on any computer or device where you can run 
+a python script.
+
+## Zipped Archived Handled
 if .csv files are in a zipped archive, the function will
 automatically find and unzip the archive and then handle each file.
+
 
 v2 2022.08.04
 """
